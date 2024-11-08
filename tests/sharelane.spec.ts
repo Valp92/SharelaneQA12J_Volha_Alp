99a920420c69eb34be2131a4b6e9eab2fec52891
Valp92 <117280818+Valp92@users.noreply.github.com>
2024-11-08 23:18:14 +0300

    fixed some points

--- tests/sharelane.spec.ts
+++ tests/sharelane.spec.ts
@@ -1,43 +1,49 @@
 import { describe } from "mocha";
 import { Browser, Builder, By, Key, until, WebDriver } from "selenium-webdriver";
 import assert from "assert";
-import { expect } from "chai";
-import { only } from "node:test";
 
 describe('Sharelane test', async () => {
 let driver: WebDriver;
+
+beforeEach(async() => {
+    console.log("pre-condition");
+    });
+    
+    afterEach(async() => {
+    console.log("post-condition");
+    });
 
 it('Sharelane test positive registration', async () => {
 driver = await new Builder().forBrowser(Browser.CHROME).build();
 await driver.manage().window().maximize();
 try {
 await driver.get('https://sharelane.com/cgi-bin/main.py');
-await driver.findElement(By.css('a[href="./register.py"]')).sendKeys(Key.RETURN);
-await driver.findElement(By.name('zip_code')).sendKeys('220121'), 5000;
-await driver.findElement(By.css('input[value="Continue"]')).sendKeys(Key.RETURN), 5000;
+await driver.findElement(By.css('a[href="./register.py"]')).click();
+await driver.findElement(By.name('zip_code')).sendKeys('220121');
+await driver.findElement(By.css('input[value="Continue"]')).click();
 await driver.wait(until.urlIs('https://sharelane.com/cgi-bin/register.py?page=1&zip_code=220121')), 5000;
-await driver.findElement(By.name('first_name')).sendKeys('Olga'), 3000;
-await driver.findElement(By.name('email')).sendKeys('testtest@gmail.com'), 3000;
-await driver.findElement(By.name('password1')).sendKeys('12345'), 3000;
-await driver.findElement(By.name('password2')).sendKeys('12345'), 3000;
-await driver.findElement(By.css('input[value="Register"]')).sendKeys(Key.RETURN), 7000;
-await driver.findElement(By.css('.confirmation_message')), 7000;
+await driver.findElement(By.name('first_name')).sendKeys('Olga');
+await driver.findElement(By.name('email')).sendKeys('testtest@gmail.com');
+await driver.findElement(By.name('password1')).sendKeys('12345');
+await driver.findElement(By.name('password2')).sendKeys('12345');
+await driver.findElement(By.css('input[value="Register"]')).click();
+await driver.findElement(By.css('.confirmation_message'));
+assert.equal(await driver.getCurrentUrl(), 'https://sharelane.com/cgi-bin/register.py?page=2&zip_code=220121&first_name=Olga&last_name=&email=testtest%40gmail.com&password1=12345&password2=12345', 'FAILED REGISTRATION');
 }
 finally {
 driver.quit();
-}
-}) 
+}}) 
 
 it('Sharelane test positive login', async () => {
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
     await driver.manage().window().maximize();
     try {
     await driver.get('https://sharelane.com/cgi-bin/main.py');
-    await driver.findElement(By.name('email')).sendKeys('testtest@gmail.com'), 5000;
-    await driver.findElement(By.name('email')).sendKeys(Key.RETURN);
-    await driver.findElement(By.name('password')).sendKeys('12345'), 5000;
-    await driver.findElement(By.name('password')).sendKeys(Key.RETURN), 5000;
-    await driver.findElement(By.css('input[value="Login"]')).sendKeys(Key.RETURN), 7000;
+    await driver.findElement(By.name('email')).sendKeys('testtest@gmail.com');
+    await driver.findElement(By.name('email')).click();
+    await driver.findElement(By.name('password')).sendKeys('12345');
+    await driver.findElement(By.name('password')).click();
+    await driver.findElement(By.css('input[value="Login"]')).click();
     }
     finally {
     driver.quit();
@@ -46,7 +52,7 @@ it('Sharelane test positive login', async () => {
 })
 
 
- it('Sharelane test negative registration', async () => {
+it('Sharelane test negative registration', async () => {
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
     await driver.manage().window().maximize();
     try {
@@ -54,7 +60,7 @@ it('Sharelane test positive login', async () => {
     await driver.findElement(By.css('a[href="./register.py"]')).sendKeys(Key.RETURN);
     await driver.findElement(By.css('input[value="Continue"]')).sendKeys(Key.RETURN), 5000;
     await driver.findElement(By.css('.error_message')), 7000;
-    }
+ }
     finally {
     driver.quit();
     }
